74eb9834c466ac35cdf342b79ca49782108987bf
annabibi <annagabriellaferreira2008@gmail.com>
2021-11-20 21:31:20 -0300

    primeira parte do projeito - menu

--- site_music.py
+++ site_music.py
@@ -1,3 +1,4 @@
+
 #www.thetoneboutique.com.br parecer com isso
 v = {'nome': 'V', 'cor': 'rosa', 'categoria': 'iniciantes', 'preço': 1000}
 mustang = {'nome': 'mustang', 'cor': 'preto', 'categoria': 'iniciantes', 'preço': 700}
@@ -8,6 +9,19 @@ guitarras.append(v)
 guitarras.append(mustang)
 guitarras.append(J_master)
 guitarras.append(SG)
+#........................................................................................................
+
+
+def guitarra_selecion(guitarras):
+  escolha = input('qual nome da guitarra que vc escolheu?')
+  for guitarra in guitarras:
+    if escolha == guitarra.get("nome"):
+      print(f'ok, vc escolheu {guitarra.get("nome")}  que custa {guitarra.get("preço")}')
+      return guitarra
+  print(f'n achei sua guitarra {escolha}')
+
+
+
 print('oi, bem vinndo')
 procurar = input('como vc vai querer procurar pela sua guitarra? pelo preço, cor, ou nome?  ')
 if procurar == 'preço':
@@ -17,6 +31,7 @@ if procurar == 'nome':
   print('temos essas guitarras:')
   for guitarra in guitarras:
     print(f'- {guitarra.get("nome")}')
+
 if procurar == 'cor':
   for guitarra in guitarras:
     print(f'essa guitarra {guitarra.get("nome")} tem cor {guitarra.get("cor")}')
